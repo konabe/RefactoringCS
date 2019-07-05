d773708afc2b0bc75c8af65175401bef3365de21
Ryota Konabe <rt.konabe@gmail.com>
2019-07-05 23:31:01 +0900

    first commit

---
+++ 6_1_Extract_Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_1_Extract_Method
+{
+    class Program
+    {
+        private readonly string _name = "Linus";
+
+        static void Main()
+        {
+            var p = new Program();
+            p.PrintOwing(2.98);
+        }
+
+        private void PrintOwing(double amount)
+        {
+            PrintBanner();
+
+            Console.WriteLine($"name: {_name}");
+            Console.WriteLine($"amount: {amount}");
+        }
+
+        private void PrintBanner()
+        {
+            Console.WriteLine("Banner");
+        }
+
+    }
+}
